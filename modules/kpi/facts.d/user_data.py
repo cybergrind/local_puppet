dd605eeb1232c5dbbd081c2141aaebb6b6fe2dc5
Kirill Pinchuk <cybergrind@gmail.com>
2016-04-27 22:54:03 +0300

    Facts: print facts about user environment

---
+++ modules/kpi/facts.d/user_data.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os
+from os.path import join, exists
+from glob import glob
+
+
+for d in glob('/home/*'):
+    user = d.replace('/home/', '')
+    stat = os.stat(d)
+    if os.fork():
+        continue
+    os.setgid(stat.st_gid)
+    os.setuid(stat.st_uid)
+    for k, v in {'id_rsa': '.ssh/id_rsa',
+                 'dropbox': 'Dropbox/wk_ssh2'}.items():
+        check_dir = join(d, v)
+        e = exists(check_dir)
+        print('{}_{}={}'.format(user, k, e))
+    break
