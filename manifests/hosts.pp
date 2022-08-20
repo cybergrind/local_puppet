fa7672f846638d07fa40ac3d4b7718245af12e22
Kirill Pinchuk <cybergrind@gmail.com>
2022-08-20 21:37:42 +0300

    puppet: class based deployment on all nodes

--- manifests/hosts.pp
+++ manifests/hosts.pp
@@ -2,22 +2,21 @@ $base = '/home'
 
 node 'home' {
   include kpi::base_devel
-  kpi::home { 'kpi': }
+  class {'kpi::home': }
 }
 
 node 'tpad' {
   include kpi::base_devel
-  kpi::home { 'kpi': }
+  class {'kpi::home': }
 }
 
 node 'dm4' {
   include kpi::base_devel
-  kpi::home { 'kpi': }
+  class {'kpi::home': }
 }
 
 node 'zz' {
   include kpi::base_devel
-
   class {'kpi::home': }
 }
 
