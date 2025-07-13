7463df0f4c813b4abc8997d6828d15db8745ff99
Kirill Pinchuk <cybergrind@gmail.com>
2025-07-13 11:01:14 +0300

    rofi: add kill process

---
+++ modules/kpi/files/home/.local/bin/rofi_kill.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rofi_dmenu() {
+    # format: 1005624 docker-compose -f infra/docker-compose.yaml up
+    kill `pidof rofi`
+    local COM=$(ps -a -u $USER --no-headers -o pid,args | sort | uniq -i | rofi -dmenu -p "   " -i)
+    local PID=$(awk '{print $1}' <<< "$COM")
+    echo "Killing process: $COM (PID: $PID)"
+    echo "$PID" | xargs -r kill
+}
+
+if [[ -z "$1" ]]; then
+   echo "Kill process"
+else
+    rofi_dmenu
+fi
